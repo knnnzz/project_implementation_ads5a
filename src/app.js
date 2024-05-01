89fb9f8c609d2779a81b74c22040af2c8c2fc0a8
Kauan <160064587+knnnzz@users.noreply.github.com>
2024-05-01 17:36:59 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
+
 const produtosRoutes = require('./routes/produto');
+const loteRoutes = require('./routes/lote');
+
 
 app.use('/api/produtos', produtosRoutes);
+app.use('/api/lotes', loteRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('Servidor ligado com sucesso!');
